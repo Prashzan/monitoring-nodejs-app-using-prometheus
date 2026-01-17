385d7f5c5e40087f81a75a9d0500e790c1bb32bb
Prashant Bhattarai <prashantbhattarai696@gmail.com>
2026-01-16 19:18:04 -0500

    creating a simple nodejs application to start monitoring with prometheus

---
+++ app/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get('/profile-picture-andrea', function (req, res) {
+  const img = fs.readFileSync(path.join(__dirname, "images/profile-andrea.jpg"));
+  res.writeHead(200, {'Content-Type': 'image/jpg' });
+  res.end(img, 'binary');
+});
+
+app.get('/profile-picture-ari', function (req, res) {
+  const img = fs.readFileSync(path.join(__dirname, "images/profile-ari.jpeg"));
+  res.writeHead(200, {'Content-Type': 'image/jpg' });
+  res.end(img, 'binary');
+});
+
+app.listen(3000, function () {
+  console.log("app listening on port 3000!");
+});
+
